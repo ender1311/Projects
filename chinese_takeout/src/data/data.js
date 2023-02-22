cc3dd4e16de7d2655963dd7da5e5785a845d09d7
ender1311 <120590237+ender1311@users.noreply.github.com>
2023-02-21 21:16:01 -0500

    updated chinese takeout

---
+++ chinese_takeout/src/data/data.js
@@ -0,0 +1,16 @@
+export const restaurantData = [
+  { name: 'Spicy Garlic Tofu', price: 10.00, img: 'https://thewoksoflife.com/wp-content/uploads/2021/03/spicy-garlic-tofu-12-1144x1536.jpg'},
+  { name: 'Chinese Congee', price: 8.00, img: 'https://thewoksoflife.com/wp-content/uploads/2016/10/congee-recipe-2.jpg'},
+  { name: 'Char Siu Chicken', price: 12.00, img: 'https://thewoksoflife.com/wp-content/uploads/2022/11/char-siu-chicken-11-1141x1536.jpg'},
+  { name: 'Vegetable Dumplings', price: 5.00, img: 'https://thewoksoflife.com/wp-content/uploads/2015/09/vegetable-dumplings-12.jpg'},
+  { name: 'Chinese Mushrooms w/ Bok Choy', price: 8.00, img: 'https://thewoksoflife.com/wp-content/uploads/2018/07/braised-chinese-mushrooms-bok-choy-9.jpg'},
+  { name: 'Beef Dumplings', price: 5.00, img: 'https://thewoksoflife.com/wp-content/uploads/2022/12/beef-dumplings-22-1200x1536.jpg'},
+  { name: 'Kung Pao Beef', price: 9.00, img: 'https://thewoksoflife.com/wp-content/uploads/2022/10/kung-pao-beef-14-1077x1536.jpg'},
+  { name: 'Vegan Ramen', price: 12.00, img: 'https://thewoksoflife.com/wp-content/uploads/2019/08/vegan-ramen-4.jpg'},
+  { name: 'Chicken w/ Chinese Broccoli', price: 10.00, img: 'https://thewoksoflife.com/wp-content/uploads/2013/08/chicken-chinese-broccoli-22-1024x1536.jpg'},
+  { name: 'Beef w/ Oyster Sauce', price: 12.00, img: 'https://thewoksoflife.com/wp-content/uploads/2022/06/beef-oyster-sauce-14-1152x1536.jpg'},
+  { name: 'Chicken Pho', price: 11.00, img: 'https://thewoksoflife.com/wp-content/uploads/2017/05/chicken-pho-2.jpg'}
+];
+
+
+export const currenciesData = ['USD', 'EUR', 'CAD'];
